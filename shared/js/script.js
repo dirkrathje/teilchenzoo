43c3358f2c39e6d9392554b0dbaff90947f27b33
Dirk Rathje <post@dirk-rathje.de>
2013-09-26 14:57:37 +0200

    removed: cordova.js

--- shared/js/script.js
+++ shared/js/script.js
@@ -393,17 +393,17 @@ function updateQuizTeaser(scrollPosition) {
     $("#quizTeaser").html(getQuizView(quizModel));
 }
 
-function onDeviceReady() {
+/*function onDeviceReady() {
     "use strict";
-/*
+
     var uuid = device.uuid;
     $("#UUID").html(uuid);
 
     if ($.inArray(uuid, particlomaticOnlyUUIDs)) {
         $("a[href='#page-home']").hide();
         $("div[data-role='footer']").hide();
-    }*/
-}
+    }
+}*/
 //document.addEventListener("deviceready", onDeviceReady, false);
 
 function onReady() {
