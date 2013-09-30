d876c82f306e7bc78d893a27f17531d042c1297a
Medina Oldenburg <medina.oldenburg@gmx.de>
2013-09-30 17:25:55 +0200

    Alle guten dinge sind...

--- animations/tauon/tauon_edgeActions.js
+++ animations/tauon/tauon_edgeActions.js
@@ -1,29 +1,12 @@
-/***********************
-* Adobe Edge Animate-Aktionen für Composition
-*
-* Bearbeiten Sie diese Datei mit Vorsicht. Achten Sie darauf, dass 
-* Funktionssignaturen und Kommentare, die mit „Edge“ anfangen, beibehalten werden, 
-* damit Sie mit diesen Aktionen weiterhin in Adobe Edge Animate interagieren können.
-*
-***********************/
-(function($, Edge, compId){
-var Composition = Edge.Composition, Symbol = Edge.Symbol; // Aliase für häufig verwendete Edge-Klassen
 
-   //Edge symbol: 'stage'
-   (function(symbolName) {
-      
-      
-      
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
+//Edge symbol: 'stage'
+(function(symbolName){})("stage");
+//Edge symbol end:'stage'
 
-   })("stage");
-   //Edge symbol end:'stage'
+//=========================================================
 
-   //=========================================================
-   
-   //Edge symbol: 'Symbol_1'
-   (function(symbolName) {   
-   
-   })("Symbol_1");
-   //Edge symbol end:'Symbol_1'
-
-})(jQuery, AdobeEdge, "EDGE-10384310");
+//Edge symbol: 'Symbol_1'
+(function(symbolName){})("Symbol_1");
+//Edge symbol end:'Symbol_1'
+})(jQuery,AdobeEdge,"EDGE-10384310");
