347ac2e1c9861236b4bb48bf78703a2a38f8fa2b
Dirk Rathje <post@dirk-rathje.de>
2013-10-19 08:54:18 +0200

    cropped 500 test

--- shared/js/script.js
+++ shared/js/script.js
@@ -292,7 +292,7 @@ function getQuizView(model) {
 
 
             tableView += "<tr>";
-            tableView += "<td class='particleThumbnail'><img src='images/particles_cropped_small/" + particle.name + ".png' alt=''/></td>";
+            tableView += "<td class='particleThumbnail'><img src='images/particles_cropped_500/" + particle.name + ".png' alt=''/></td>";
 //            tableView += "<td class='particleThumbnail'><img src='images/particles_cropped/top_quark.png' alt=''/></td>";
             tableView += "<td class='value'><div id='resultBin"+ particle.name + "' class='resultBin "+ particle.name + "' style='background-color: " + particle.rgb + ";width: " + normalizedValue + "%;'></td>";
             tableView += "</tr>";
