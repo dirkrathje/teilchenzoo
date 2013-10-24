815f1d20d778905ade94e47feaa3569628ac92a7
Dirk Rathje <post@dirk-rathje.de>
2013-10-24 18:12:53 +0200

    edge animate experiments

--- shared/animations/electron/electron_edgeActions.js
+++ shared/animations/electron/electron_edgeActions.js
@@ -1,35 +1,23 @@
-/***********************
-* Adobe Edge Animate-Aktionen für Composition
-*
-* Bearbeiten Sie diese Datei mit Vorsicht. Achten Sie darauf, dass 
-* Funktionssignaturen und Kommentare, die mit „Edge“ anfangen, beibehalten werden, 
-* damit Sie mit diesen Aktionen weiterhin in Adobe Edge Animate interagieren können.
-*
-***********************/
-(function($, Edge, compId){
-var Composition = Edge.Composition, Symbol = Edge.Symbol; // Aliase für häufig verwendete Edge-Klassen
 
-   //Edge symbol: 'stage'
-   (function(symbolName) {
-      
-      
-      
-
-      Symbol.bindTriggerAction(compId, symbolName, "Default Timeline", 21350, function(sym, e) {
-         sym.play();
-
-      });
-      //Edge binding end
-
-   })("stage");
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindTriggerAction(compId, symbolName, "Default Timeline", 21350, function(sym,e){sym.play();});
+//Edge binding end
+})("stage");
    //Edge symbol end:'stage'
+
+//=========================================================
+
+//Edge symbol: 'electron'
+(function(symbolName){})("electron");
+   //Edge symbol end:'electron'
 
    //=========================================================
    
-   //Edge symbol: 'electron'
+   //Edge symbol: 'Preloader'
    (function(symbolName) {   
    
-   })("electron");
-   //Edge symbol end:'electron'
+   })("Preloader");
+   //Edge symbol end:'Preloader'
 
-})(jQuery, AdobeEdge, "EDGE-2207778");
+})(jQuery, AdobeEdge, "edgeComposition-electron");
